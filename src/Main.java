1dea4fa922f39039c0f424f63799ef20a2370f0e
Peter Lind <petl@kea.dk>
2022-04-04 21:31:56 +0200

    added Main class with runnable main code

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Cat cat = new Cat("Mr. Miav");
+        Dog dog = new Dog("Vuffer");
+
+        cat.makeSound();
+        dog.makeSound();
+    }
+}
