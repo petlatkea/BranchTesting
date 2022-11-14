8c063fec39abffb73ce995fed724aba4c896338e
Peter Lind <petl@kea.dk>
2022-11-14 12:51:24 +0100

    added makeSound

--- src/Animal.java
+++ src/Animal.java
@@ -1,3 +1,5 @@
 public abstract class Animal {
     private String name;
+
+    public abstract void makeSound();
 }
--- src/Dog.java
+++ src/Dog.java
@@ -1,2 +1,6 @@
 public class Dog extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("Vuf!");
+    }
 }
