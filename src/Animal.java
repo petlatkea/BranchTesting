33ca4871b929903719f0402fd730d7b2f01d8586
Peter Lind <petl@kea.dk>
2022-11-14 13:17:33 +0100

    add eat-method

--- src/Animal.java
+++ src/Animal.java
@@ -2,4 +2,6 @@ public abstract class Animal {
     private String name;
 
     public abstract void makeSound();
+
+    public abstract boolean eat(String foodType);
 }
--- src/Cat.java
+++ src/Cat.java
@@ -3,4 +3,9 @@ public class Cat extends Animal {
     public void makeSound() {
         System.out.println("Miav ...");
     }
+
+    @Override
+    public boolean eat(String foodType) {
+        return true;
+    }
 }
